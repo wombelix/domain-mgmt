066c4fff749fab6ed15b44eab651f65f91dbe414
Dominik Wombacher <dominik@wombacher.cc>
2025-02-10 10:57:22 +0100

    feat(domain): add mail2api.de

---
+++ domain_mail2api_de.tf
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "mail2api_de" {
+  source      = "./modules/domains"
+  domain_name = "mail2api.de"
+}
