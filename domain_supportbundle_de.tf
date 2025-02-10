ca52dd835889800ec2ea91dbd44bf12a9fdc6631
Dominik Wombacher <dominik@wombacher.cc>
2025-02-10 10:57:26 +0100

    feat(domain): add supportbundle.de

---
+++ domain_supportbundle_de.tf
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "supportbundle_de" {
+  source      = "./modules/domains"
+  domain_name = "supportbundle.de"
+}
