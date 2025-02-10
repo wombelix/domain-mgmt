66293acdbf6aee2b003f2d8d522c679051cc20c0
Dominik Wombacher <dominik@wombacher.cc>
2025-02-10 10:58:10 +0100

    feat(domain): add xlii.cc

---
+++ domain_xlii_cc.tf
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "xlii_cc" {
+  source              = "./modules/domains"
+  domain_name         = "xlii.cc"
+  contacts_registrant = "487953"
+  contacts_admin      = "487953"
+  caa_iodef_mailto    = "dominik@wombacher.cc"
+  automated_dnssec    = false
+}
