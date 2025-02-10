60fc9b8debfc489a40184a9083bd4cd7ab1147db
Dominik Wombacher <dominik@wombacher.cc>
2025-02-10 10:56:57 +0100

    feat(domain): add sosreport.de

---
+++ domain_sosreport_de.tf
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "sosreport_de" {
+  source      = "./modules/domains"
+  domain_name = "sosreport.de"
+}
