e70dfb9e596bb5667f8b92e482ef2ae8b7f59209
Dominik Wombacher <dominik@wombacher.cc>
2025-07-20 21:27:25 +0200

    feat(domain): update admin/registrant of wombacher.dev

--- domain_wombacher_dev.tf
+++ domain_wombacher_dev.tf
@@ -3,6 +3,13 @@
 # SPDX-License-Identifier: MIT
 
 module "wombacher_dev" {
-  source      = "./modules/domains"
-  domain_name = "wombacher.dev"
+  source              = "./modules/domains"
+  domain_name         = "wombacher.dev"
+  contacts_registrant = "487953"
+  contacts_admin      = "487953"
+  caa_iodef_mailto    = "dominik@wombacher.cc"
+  extra_data = {
+    "ACKNOWLEDGE-SECURE-ONLY-DEV" = "1"
+  }
+
 }
