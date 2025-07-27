2407498487f86e477acd7ee96d4bb92f19628842
Dominik Wombacher <dominik@wombacher.cc>
2025-07-27 12:56:53 +0200

    feat(glue): drop ns glue records for mycloudoffice.de, not used as nameserver set anymore, all domains migrated to wombacher.it nameservers

--- glue_records.tf
+++ glue_records.tf
@@ -2,31 +2,6 @@
 #
 # SPDX-License-Identifier: MIT
 
-# Glue records for mycloudoffice.de nameservers
-resource "inwx_glue_record" "dns1_mycloudoffice_de" {
-  hostname = "dns1.mycloudoffice.de"
-  ip = [
-    "192.174.68.104",
-    "2001:67c:1bc::104"
-  ]
-}
-
-resource "inwx_glue_record" "dns2_mycloudoffice_de" {
-  hostname = "dns2.mycloudoffice.de"
-  ip = [
-    "176.97.158.104",
-    "2001:67c:10b8::104"
-  ]
-}
-
-resource "inwx_glue_record" "dns3_mycloudoffice_de" {
-  hostname = "dns3.mycloudoffice.de"
-  ip = [
-    "45.87.158.53",
-    "2a02:d500::53"
-  ]
-}
-
 # Glue records for wombacher.it nameservers
 resource "inwx_glue_record" "ns1_wombacher_it" {
   hostname = "ns1.wombacher.it"
