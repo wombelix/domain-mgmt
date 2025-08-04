14fac9238b3d399a47aeaad97e60f01bd8234c2e
Dominik Wombacher <dominik@wombacher.cc>
2025-08-05 00:35:01 +0200

    feat(domain): register sosgenie.de/.eu

---
+++ domain_sosgenie_de.tf
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "sosgenie_de" {
+  source      = "./modules/domains"
+  domain_name = "sosgenie.de"
+}
