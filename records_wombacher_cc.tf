df1e2f7eedec646309d535808458d82084ccb2a4
Dominik Wombacher <dominik@wombacher.cc>
2025-10-06 12:37:59 +0200

    feat(records): wombacher.cc, bump mail related ttl to 3600 after completed migration

--- records_wombacher_cc.tf
+++ records_wombacher_cc.tf
@@ -39,7 +39,7 @@ resource "inwx_nameserver_record" "wombacher_cc_cname_autoconfig" {
   name    = "autoconfig.wombacher.cc"
   type    = "CNAME"
   content = "mail.your-server.de"
-  ttl     = 300
+  ttl     = 3600
 }
 
 resource "inwx_nameserver_record" "wombacher_cc_txt_default2501_domainkey" {
@@ -47,7 +47,7 @@ resource "inwx_nameserver_record" "wombacher_cc_txt_default2501_domainkey" {
   name    = "default2501._domainkey.wombacher.cc"
   type    = "TXT"
   content = "v=DKIM1; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArExTtKHVZGJJ4S11LJmeDU6AWY9z5At9yThjzIz9mBORUIrZ/OztUoTmN6T+STMaQ/mVmP3qUpwUcnBv5LE+OezW8ec7EsQOZ0AvNuHNKQerRLXbLlzjNeoCCUY1QR9k165PNFi/paL31n7vIs6mS+prBmBOrQY3rk6jrY/SXF3qfFaT8jQ5UFZR5nbDcKPTFOGPAQn7fzTTr8pGwC/48COUt1rJdwSsIUewVpxNODsLhZLELs623oSHFh5JZzo+4fJo1w8Yc8XRgZns8KcfAk+KuetxSR23jdA6sd6NorXycdfC5ELFMFIzNyZjOXSkC/4uNfwAqXvgkMkZmuHBrwIDAQAB"
-  ttl     = 300
+  ttl     = 3600
 }
 
 resource "inwx_nameserver_record" "wombacher_cc_cname_www" {
@@ -64,7 +64,7 @@ resource "inwx_nameserver_record" "wombacher_cc_mx_www348" {
   type    = "MX"
   prio    = 10
   content = "www348.your-server.de"
-  ttl     = 300
+  ttl     = 3600
 }
 
 resource "inwx_nameserver_record" "wombacher_cc_srv_autodiscover_tcp" {
@@ -73,7 +73,7 @@ resource "inwx_nameserver_record" "wombacher_cc_srv_autodiscover_tcp" {
   type    = "SRV"
   content = "100 443 mail.your-server.de"
   prio    = 0
-  ttl     = 300
+  ttl     = 3600
 }
 
 resource "inwx_nameserver_record" "wombacher_cc_srv_imaps_tcp" {
@@ -82,7 +82,7 @@ resource "inwx_nameserver_record" "wombacher_cc_srv_imaps_tcp" {
   type    = "SRV"
   content = "100 993 mail.your-server.de"
   prio    = 0
-  ttl     = 300
+  ttl     = 3600
 }
 
 resource "inwx_nameserver_record" "wombacher_cc_srv_pop3s_tcp" {
@@ -91,7 +91,7 @@ resource "inwx_nameserver_record" "wombacher_cc_srv_pop3s_tcp" {
   type    = "SRV"
   content = "100 995 mail.your-server.de"
   prio    = 0
-  ttl     = 300
+  ttl     = 3600
 }
 
 resource "inwx_nameserver_record" "wombacher_cc_srv_submission_tcp" {
@@ -100,7 +100,7 @@ resource "inwx_nameserver_record" "wombacher_cc_srv_submission_tcp" {
   type    = "SRV"
   content = "100 587 mail.your-server.de"
   prio    = 0
-  ttl     = 300
+  ttl     = 3600
 }
 
 resource "inwx_nameserver_record" "wombacher_cc_txt_dominik_1" {
@@ -116,7 +116,7 @@ resource "inwx_nameserver_record" "wombacher_cc_txt_2" {
   name    = ""
   type    = "TXT"
   content = "v=spf1 a mx ~all"
-  ttl     = 300
+  ttl     = 3600
 }
 
 resource "inwx_nameserver_record" "wombacher_cc_txt_3" {
@@ -132,5 +132,5 @@ resource "inwx_nameserver_record" "wombacher_cc_txt__dmarc" {
   name    = "_dmarc.wombacher.cc"
   type    = "TXT"
   content = "v=DMARC1; p=none; rua=mailto:postmaster@wombacher.cc; ruf=mailto:postmaster@wombacher.cc; rf=afrf; sp=none; pct=100; fo=1; ri=86400; adkim=r; aspf=r"
-  ttl     = 300
+  ttl     = 3600
 }
