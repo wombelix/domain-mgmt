338223876cc52e4f893030577a02da40677ce9e3
Dominik Wombacher <dominik@wombacher.cc>
2026-01-13 23:41:51 +0100

    feat(tf): Migrate to new KMS key and state backend
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- terraform.tfvars
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
-#
-# SPDX-License-Identifier: MIT
-
-project = "domain-mgmt"
--- variables.tf
+++ variables.tf
@@ -4,31 +4,48 @@
 
 variable "aws_region" {
   type        = string
-  description = "AWS Region for tf encryption and backend"
+  description = "AWS Region to use"
+  default     = "eu-central-1"
 }
 
 variable "aws_partition" {
   type        = string
+  description = "AWS Partition to use"
   default     = "aws"
-  description = "AWS Partition for tf encryption and backend"
 }
 
 variable "aws_account_id" {
   type        = string
-  description = "AWS Account ID for tf encryption and backend"
+  description = "AWS Account ID with tf remote backend and state encryption resources"
+  default     = "992382418145"
 }
 
 variable "aws_kms_name" {
   type        = string
-  description = "AWS KMS name for tf encryption and backend"
+  description = "AWS KMS name for tf encryption"
+  default     = "mrk-9888e66a1d1d4d4c8c92b2a3d3cd2b85"
 }
 
 variable "aws_s3_bucket" {
   type        = string
-  description = "AWS S3 Bucket name for tf encryption and backend"
+  description = "AWS S3 Bucket name for tf remote backend"
+  default     = "dominik-wombacher-iac-opentofu"
+}
+
+variable "aws_s3_bucket_state_prefix" {
+  type        = string
+  description = "AWS S3 Bucket prefix for tf states"
+  default     = "opentofu-states"
+}
+
+variable "aws_dynamodb_table" {
+  type        = string
+  description = "AWS DynamoDB Table name for tf backend locking"
+  default     = "dominik-wombacher-iac-opentofu"
 }
 
 variable "project" {
   type        = string
   description = "OpenTofu IaC project name"
+  default     = "domain-mgmt"
 }
