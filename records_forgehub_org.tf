99e41b4ce5912d2efba763ef0f10a2c81554dfaf
Dominik Wombacher <dominik@wombacher.cc>
2026-03-11 17:36:06 +0100

    feat(records): forgehub.org records to harden mail
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

---
+++ records_forgehub_org.tf
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2026 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# TXT Records
+resource "inwx_nameserver_record" "forgehub_org_txt_spf" {
+  domain  = "forgehub.org"
+  name    = "forgehub.org"
+  type    = "TXT"
+  content = "v=spf1 -all"
+  ttl     = 300
+}
+
+resource "inwx_nameserver_record" "forgehub_org_txt_dmarc" {
+  domain  = "forgehub.org"
+  name    = "_dmarc.forgehub.org"
+  type    = "TXT"
+  content = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; pct=100"
+  ttl     = 300
+}
