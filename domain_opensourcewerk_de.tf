a8bf7f4c4fa042cb67ee83dec6029c641eab50c2
Dominik Wombacher <dominik@wombacher.cc>
2026-05-24 12:57:01 +0200

    feat(domain): delete opensourcewerk.de
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- domain_opensourcewerk_de.tf
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2026 Dominik Wombacher <dominik@wombacher.cc>
-#
-# SPDX-License-Identifier: Apache-2.0
-
-module "opensourcewerk_de" {
-  source       = "./modules/domains"
-  domain_name  = "opensourcewerk.de"
-  renewal_mode = "AUTODELETE"
-}
