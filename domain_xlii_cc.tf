0d75bda617c6630bb339c10519399c28d395e2c3
Dominik Wombacher <dominik@wombacher.cc>
2026-05-24 12:57:32 +0200

    feat: delete domain and records - xlii.cc
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- domain_xlii_cc.tf
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2026 Dominik Wombacher <dominik@wombacher.cc>
-#
-# SPDX-License-Identifier: Apache-2.0
-
-module "xlii_cc" {
-  source       = "./modules/domains"
-  domain_name  = "xlii.cc"
-  renewal_mode = "AUTODELETE"
-}
